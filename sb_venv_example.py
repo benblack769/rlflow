c000e63bc9e0d803a850d530f5fbcb2dff3d7682
Ben Black <weepingwillowben@gmail.com>
2020-08-21 16:42:26 -0400

    updated sb venv example

--- sb_venv_example.py
+++ sb_venv_example.py
@@ -1,39 +1,79 @@
 from rlflow.vector.aec_markov_wrapper import aec_to_markov
-from rlflow.vector.dummy_vec_env import DummyVecEnv
-from rlflow.vector.multiproc_vec import ProcConcatVec
-from rlflow.vector.concat_vec_env import ConcatVecEnv
-from rlflow.vector.markov_vector_wrapper import MarkovVectorEnv
-from rlflow.vector.sb_vector_wrapper import VecEnvWrapper
+#from rlflow.vector import DummyVecEnv
+from rlflow.vector import ProcConcatVec
+from rlflow.vector import ConcatVecEnv
+from rlflow.vector import MarkovVectorEnv
+from rlflow.vector import VecEnvWrapper,MakeCPUAsyncConstructor
 
 from pettingzoo.sisl import multiwalker_v0
 from supersuit.aec_wrappers import pad_observations, pad_action_space
-from stable_baselines3 import A2C
+from stable_baselines3 import PPO, A2C
+import time
+from stable_baselines3.common.utils import safe_mean
+from stable_baselines3.common.logger import ScopedConfigure, configure
+from stable_baselines3.common import logger
+import stable_baselines3
+
+from rlflow.utils.logger import make_logger
 #from stable_baselines.acktr import MlpPolicy
 import gym
 
 def main():
     def env_contr():
-        #env = gym.make("CartPole-v0")#
-        env = multiwalker_v0.env()
-        env = pad_observations(env)
-        env = pad_action_space(env)
-        markov_env = aec_to_markov(env)
-        venv = MarkovVectorEnv(markov_env)
-        return venv
-    envs_per_proc = 4
-    n_procs = 4
-    def nest_env_const():
-        cat = ConcatVecEnv([env_contr]*envs_per_proc)
-        return cat
+        return gym.make("CartPole-v0")#
+        # env = multiwalker_v0.env()
+        # env = pad_observations(env)
+        # env = pad_action_space(env)
+        # markov_env = aec_to_markov(env)
+        # venv = MarkovVectorEnv(markov_env)
+        # return venv
+    n_envs = 6
+    # def nest_env_const():
+    #     cat = ConcatVecEnv([env_contr]*envs_per_proc)
+    #     return cat
     example_env = env_contr()
-    num_envs = n_procs*envs_per_proc*example_env.num_envs
+    num_envs =n_envs*1#example_env.num_envs
     #cat = ProcConcatVec([nest_env_const]*n_procs,example_env.observation_space, example_env.action_space, num_envs)
-    cat = ConcatVecEnv([nest_env_const]*n_procs)#,example_env.observation_space, example_env.action_space, num_envs)
+    cat = MakeCPUAsyncConstructor(0)([env_contr]*n_envs,example_env.observation_space, example_env.action_space)#, num_envs)
     cat = VecEnvWrapper(cat)
+    env = cat
     policy = "MlpPolicy"
-    a2c = A2C(policy,cat)
+    logger = make_logger("log")
+    stable_baselines3.common.logger.Logger.CURRENT = logger
+    a2c = PPO(policy,cat,n_steps=4,batch_size=6,n_epochs=3)
     print(type(a2c.env))
-    a2c.learn(1000000,log_interval=2,eval_freq=100000000000000)
+    #a2c.learn(1000000)
+
+    total_timesteps, callback = a2c._setup_learn(
+        10000, None, None, None, n_eval_episodes=5, reset_num_timesteps=None, tb_log_name="PPo"
+    )
+
+    #total_timesteps = 100
+    iteration = 0
+    log_interval = 1
+    for i in range(total_timesteps):
+        continue_training = a2c.collect_rollouts(env, callback, a2c.rollout_buffer, n_rollout_steps=a2c.n_steps)
+        print(a2c.ep_info_buffer)
+        if continue_training is False:
+            break
+
+        iteration += 1
+        a2c._update_current_progress_remaining(a2c.num_timesteps, total_timesteps)
+
+        # Display training infos
+        if log_interval is not None and iteration % log_interval == 0:
+            fps = int(a2c.num_timesteps / (time.time() - a2c.start_time))
+            logger.record("time/iterations", iteration, exclude="tensorboard")
+            print(a2c.ep_info_buffer)
+            if len(a2c.ep_info_buffer) > 0 and len(a2c.ep_info_buffer[0]) > 0:
+                logger.record("rollout/ep_rew_mean", safe_mean([ep_info["r"] for ep_info in a2c.ep_info_buffer]))
+                logger.record("rollout/ep_len_mean", safe_mean([ep_info["l"] for ep_info in a2c.ep_info_buffer]))
+            logger.record("time/fps", fps)
+            logger.record("time/time_elapsed", int(time.time() - a2c.start_time), exclude="tensorboard")
+            logger.record("time/total_timesteps", a2c.num_timesteps, exclude="tensorboard")
+            logger.dump(step=a2c.num_timesteps)
+
+        a2c.train()
 
     # obs = cat.reset()
     # print("obs_shape")
@@ -42,6 +82,9 @@ def main():
     #     obs,rew,done,info = cat.step([0]*cat.num_envs)
     # print(obs.shape)
     # print(rew.shape)
+
+configure("log", format_strings='stdout,log,csv'.split(','))
+#with ScopedConfigure("log", format_strings='stdout,log,csv'.split(',')):
 main()
 #res = ProcConcatVec([env_contr])
 # env = aec_to_markov(env)
